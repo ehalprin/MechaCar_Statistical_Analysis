dc4303c3cca3eb1ad8c66f1a77644300db361c14
Elizabeth Halprin <elizabethhalprin@elizabeths-air.lan>
2022-09-11 13:11:46 -0500

    Updating with Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,6 +5,11 @@ model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clear
 summary(model) # get summary of linear regression to determine p-value and r-squared value
 
 mecha_coil <- read.csv('Suspension_Coil.csv') # read in CSV as dataframe
-total_summary <- mecha_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
-lot_summary <- mecha_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+total_summary <- mecha_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) # create dataframe of summary statistics for all lots
+lot_summary <- mecha_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) # create dataframe of summary statistics by lot
+
+t.test(mecha_coil$PSI, mu=1500) # test if mean PSI of sample is statistically different than pop. mean
+t.test(subset(mecha_coil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+t.test(subset(mecha_coil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+t.test(subset(mecha_coil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
 
